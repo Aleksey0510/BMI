c9a0c7034f7a114637cbb5f95524b07067f4bb08
Aleksey <kolesov000@mail.ru>
2024-12-13 23:44:15 +0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        BmiService service = new BmiService();
+        double weight = 84.4; // вес в кг
+        double height = 1.74; // рост в м
+        int result = service.calculate(weight, height);
+        System.out.println(result);
+
     }
 }
